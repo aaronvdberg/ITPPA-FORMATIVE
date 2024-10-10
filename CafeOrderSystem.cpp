ce37917f2332d3403316193d0d997b7a075b73d5
aaronvdberg <avdbergbusiness@gmail.com>
2024-10-10 21:29:02 +0200

    added menu items

--- CafeOrderSystem.cpp
+++ CafeOrderSystem.cpp
@@ -1,6 +1,7 @@
 // my first program in C++
 #include <iostream>
 #include <string>
+#include <unistd.h>
 
 const double COFFEE_PRICE = 15.00;
 const double SANDWICH_PRICE = 30.00;
@@ -27,9 +28,24 @@ std::cout << "Welcome, " << displayName << " " << displayLName << std::endl;
 
 return;
 }
+
+void displayMenu ()
+{
+	sleep(1);
+	std::cout << "Cafeteria Menu:" << "\n" << std::endl;
+        std::cout << "1. " << "Coffee" << " - " << "R15.00" << "\n" << std::endl;
+        std::cout << "2. " << "Sandwich" << " - " << "R30.00" << "\n" << std::endl;
+	std::cout << "3. " << "Salad" << " - " << "R25.00" << "\n" << std::endl;
+	std::cout << "4. " << "Juice" << " - " << "R10.00" << "\n" << std::endl;
+	std::cout << "5. " << "Muffin" << " - " << "R20.00" << "\n" << std::endl;
+	std::cout << "6. " << "Pizza Slice" << " - " << "R35.00" << "\n" << std::endl;
+	std::cout << "7. " << "Soup" << " - " << "R18.00" << "\n" << std::endl;
+	std::cout << "8. " << "Burger" << " - " << "R40.00" << "\n" << std::endl;
+}
+
 int main()
 {
 
 	displayNames();
-
+	displayMenu();
 }
